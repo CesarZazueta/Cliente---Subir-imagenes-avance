8f4ac06fd0614d9ffd5e788079fc6dc3d6cf37b8
CesarZazueta <cesaraory12321@gmail.com>
2023-05-12 00:09:39 -0700

    ahora obtengo la el buffer de la imagen

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 var formulario = document.getElementById('SubirImagen');
+var formulario2 = document.getElementById('ObtenerImagen');
 
- formulario = addEventListener('submit', async function(e){
+ formulario.addEventListener('submit', async function(e){
     e.preventDefault();
     console.log('me diste un click')
 
     var datos = new FormData(SubirImagen);
     datos.append('numeroaleatorio',123456);
-    datos.append('hash','98ed7105be7351f7ca847b4628a28b72bcf5e2d2d016a14154fd9ae7a5501b6e');
+    datos.append('hash','43ad64c50a1c13836db424372343498ca6b35760f86a03274db4617709c5037b');
     console.log(datos.get('numerosaleatorio'));
 
 
@@ -30,12 +31,31 @@ var formulario = document.getElementById('SubirImagen');
 
 })
 
-var ObtenerImagen = document.getElementById('ObtenerImagen');
-ObtenerImagen = addEventListener('submit', async function(e){
+formulario2.addEventListener('submit', async function(e){
     e.preventDefault();
-    console.log('me diste un click')
+    console.log('hola')
 
-    var datos = new FormData(SubirImagen);
+    var datos = new FormData(ObtenerImagen);
+    let res = await fetch("http://127.0.0.1:4000/obtenerimagenes",{
+        method: 'POST',
+        body: datos
+    });
+
+    let respuesta;
+     if (res.ok) {  // si el HTTP-status es 200-299 // obtener cuerpo de la respuesta (método debajo)
+        respuesta = await res.text();
+        console.log(respuesta);
+    } else {
+        alert("Error-HTTP: " + res.status);
+    }
+
+    let respuestaJson = JSON.parse(respuesta);
+    console.log(respuestaJson);
+    console.log(respuestaJson.data.imageBuffer.data);
+
+    // idImagen = respuestaJson.data;
+    // console.log(respuestaJson.data.idpeticion);
+    // document.getElementById("NumeroID").value = idImagen;
 
 
 })
